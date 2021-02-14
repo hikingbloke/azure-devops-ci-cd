2cf386b1a71e3573e23aa3de79fba69f43229afc
Bipin Sawant <bipin.sawant@gmail.com>
2021-02-14 01:50:52 -0800

    updating host name

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def predict():
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=443, debug=True)
+    app.run(host='https://flaskhellobipin.azurewebsites.net', port=443, debug=True)
